e169aa5dd5ec1fe024dd105149a3a685d81e3e4f
jenifer2015 <31485128+jenifer2015@users.noreply.github.com>
2017-11-22 13:30:13 -0600

    Add files via upload

---
+++ coceno.cc
@@ -0,0 +1,14 @@
+//Dorantes Garcia Jenifer
+//21 de noviembre  del 2017
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+float x;
+cout << "Ingrese un numero: ";
+ cin >> x;
+float coseno=cos(x);
+cout << "El coseno de x es " << coseno;
+return 0;
+}
+
